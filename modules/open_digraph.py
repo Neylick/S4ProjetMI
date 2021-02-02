9af0a6e06efce2b6c46e26f8fdec36cb1016d469
Neylick <neylick@gmail.com>
2021-02-02 17:50:10 +0100

    td2-jusqu'à exo4 compris, à tester

--- modules/open_digraph.py
+++ modules/open_digraph.py
@@ -15,13 +15,43 @@ class node:
     '''
     return the string corresponding to the current node
     '''
-    return "("+str(self.id)+", '"+self.label+"', "+str(self.parents)+", "+str(self.children)+")"
+    return "(" + str(self.id) + ", '" +self.label + "', " +str(self.parents)+", "+str(self.children)+")"
 
   def __repr__(self):
     return "node"+str(self)
 
   def copy(self):
-    return node(self.id, self.label, self.parents, self.children)
+    return node(self.id, self.label, self.parents.copy(), self.children.copy())
+
+  def get_id(self):
+    return self.id 
+
+  def get_label(self):
+    return self.label
+  
+  def get_parent_ids(self):
+    return self.parents
+  
+  def get_children_ids(self):
+    return self.children
+
+  def set_id(self, _id):
+    self.id = _id
+
+  def set_label(self, _label):
+    self.label = _label
+
+  def set_parent_ids(self, _ids):
+    self.parents = _ids
+  
+  def set_children_ids(self, _ids):
+    self.children = _ids
+
+  def add_child_id(self, _id):
+    self.children.append(_id)
+
+  def add_parent_id(self, _id):
+    self.parents.append(_id)
 
 class open_digraph: # for open directed graph
   def __init__(self, inputs, outputs, nodes):
@@ -47,11 +77,56 @@ class open_digraph: # for open directed graph
     return open_digraph([],[],{})
 
   def copy(self):
-    return open_digraph(self.inputs, self.outputs, {n.id:n.copy() for n in self.nodes})
+    return open_digraph(self.inputs, self.outputs, [self.nodes[k].copy() for k in self.nodes])
 
+  def get_input_ids(self):
+    return self.inputs
+  
+  def get_output_ids(self):
+    return self.outputs
+  
+  def get_id_node_map(self):
+    return self.nodes
+  
+  def get_nodes(self):
+    return [self.nodes[k] for k in self.nodes]
+  
+  def get_nodes_ids(self):
+    return [k for k in self.nodes]
+
+  def get_node_by_id(self, _id):
+    return self.nodes[_id]
+
+  def get_nodes_by_ids(self, _ids):
+    return [self.nodes[k] for k in _ids]
+
+  def set_input_ids(self, _ids):
+    self.inputs = _ids
+  
+  def set_output_ids(self, _ids):
+    self.outputs = _ids
+
+  def add_input_id(self, _id):
+    self.inputs.append(_id)
+
+  def add_output_id(self, _id):
+    self.outputs.append(_id)
+
+  def new_id(self):
+    if len(node) == 0 : return 0
+    else :
+      nextid = float('-inf')
+      for n in self.nodes :
+        if n.id > nextid : nextid = n.id+1
+      return nextid
+
+
+
+'''
 n0=node(0,'a',[],[1])
 n1=node(1,'b',[1],[0])
 g=open_digraph([0],[1],[n0,n1])
 print(n0)
 print(g)
 print(open_digraph.empty())
+'''
--- tests/open_digraph_test.py
+++ tests/open_digraph_test.py
@@ -1,5 +1,5 @@
 import sys
-sys.path.append('../') # allows us to fetch files from the project root
+sys.path.append('./../') # allows us to fetch files from the project root
 import unittest
 from modules.open_digraph import *
 
@@ -16,11 +16,95 @@ class InitTest(unittest.TestCase):
   def test_init_digraph(self):
     n0 = node(0, 'i', [], [1])
     n1 = node(1,'r',[0],[])
-    g = open_digraph([n0],[n1],[n0,n1])
+    g = open_digraph([0],[1],[n0,n1])
     self.assertEqual(g.nodes, {0:n0,1:n1})
-    self.assertEqual(g.inputs, [n0])
-    self.assertEqual(g.outputs, [n1])
+    self.assertEqual(g.inputs, [0])
+    self.assertEqual(g.outputs, [1])
     self.assertIsInstance(g, open_digraph)
-    
+
+class NodeTest(unittest.TestCase):
+  def setUp(self):
+    self.n0 = node(0, 'a', [], [1])
+    self.n1 = node(1,'b',[0],[])
+  
+  def test_string(self):
+    self.assertEqual(self.n0.__str__(), "(0, 'a', [], [1])")
+    self.assertEqual(self.n1.__str__(), "(1, 'b', [0], [])")
+  
+  def test_copy(self):
+    newnode = self.n0.copy()
+    newnode.label = 'n'
+    self.assertNotEqual(newnode.label, self.n0.label)
+    self.assertIsNot(self.n0,newnode)
+    self.assertEqual(self.n0.id, newnode.id)
+
+  def test_gets(self):
+    self.assertEqual(self.n0.get_children_ids(), self.n0.children)
+    self.assertEqual(self.n0.get_parent_ids(), self.n0.parents)
+    self.assertEqual(self.n0.get_id(), self.n0.id)
+    self.assertEqual(self.n0.get_label(), self.n0.label)
+
+  def test_sets(self):
+    self.n0.set_children_ids([1,2])
+    self.assertEqual(self.n0.children, [1,2])
+    self.n0.set_children_ids([])
+    self.assertEqual(self.n0.children, [])
+    self.n0.set_parent_ids([1,2])
+    self.assertEqual(self.n0.parents, [1,2])
+    self.n0.set_parent_ids([])
+    self.assertEqual(self.n0.parents, [])
+    self.n0.set_id(-999)
+    self.assertEqual(self.n0.id, -999)
+    self.n0.set_label('test')
+    self.assertEqual(self.n0.label, 'test')
+
+  def test_adds(self):
+    self.n0.add_child_id(999)
+    self.assertIn(999, self.n0.children)
+    self.n0.add_parent_id(999)
+    self.assertIn(999, self.n0.parents)
+
+class DigraphTest(unittest.TestCase):
+  def setUp(self):
+    n0 = node(0, 'i', [], [1])
+    n1 = node(1,'r',[0],[])
+    self.g = open_digraph([0],[1],[n0,n1])
+
+  def test_string(self):
+    self.assertEqual(self.g.__str__(), "([0], [1], {0: node(0, 'i', [], [1]), 1: node(1, 'r', [0], [])})")
+
+  def test_copy(self):
+    gbis = self.g.copy()
+    self.assertIsNot(self.g, gbis)
+    self.assertIsNot(self.g.nodes, gbis.nodes)
+    self.assertIs(self.g.inputs, gbis.inputs) 
+    self.assertIs(self.g.outputs, gbis.outputs) 
+
+  def test_gets(self):
+    self.assertEqual(self.g.get_input_ids, self.g.inputs)
+    self.assertEqual(self.g.get_output_ids(), self.g.outputs)
+    self.assertEqual(self.g.get_id_node_map(), self.g.nodes)
+    self.assertEqual(self.g.get_nodes(), [self.g.nodes[k] for k in self.g.nodes])
+    self.assertEqual(self.g.get_nodes_ids(), [k for k in self.g.nodes])
+    self.assertEqual(self.g.get_nodes_by_ids(self.g.nodes), [self.g.nodes[k] for k in self.g.nodes])
+    self.assertEqual(self.g.get_node_by_id(self.g.nodes[0]), self.g.nodes[0])
+
+  def test_sets(self):
+    self.g.set_input_ids([1,2])
+    self.assertEqual(self.g.inputs, [1,2])
+    self.g.set_input_ids([])
+    self.assertEqual(self.g.inputs, [])
+    self.g.set_output_ids([])
+    self.assertEqual(self.g.outputs, [])
+    self.g.set_output_ids([1,2])
+    self.assertEqual(self.g.outputs, [1,2])
+
+  def test_adds(self):
+    self.g.add_input_id(-999)
+    self.assertIn(-999, self.g.inputs)
+    self.g.add_output_id(-999)
+    self.assertIn(-999, self.g.outputs)
+
+
 if __name__ == '__main__': # the following code is called only when
   unittest.main() # precisely this file is run 
